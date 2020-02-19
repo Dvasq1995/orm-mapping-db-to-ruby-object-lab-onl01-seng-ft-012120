d9cc2bf0032ee82bca5868f4c6c7dd0455a1650d
Diego Vasquez <botellongo69@outlook.com>
2020-02-19 19:13:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,7 @@ class Student
   end
   
   def self.all_students_in_grade_9
-    sql = "SELECT * FROM students WHERE grade = ;"
+    sql = "SELECT * FROM students WHERE grade = 9;"
   end
   
   def self.students_below_12th_grade
