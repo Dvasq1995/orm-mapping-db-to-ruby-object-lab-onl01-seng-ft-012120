02edd79b2400147ad4ccea8e0b08b6bbdf3b30c0
Diego Vasquez <botellongo69@outlook.com>
2020-02-19 19:15:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,7 +65,7 @@ class Student
   end
   
   def self.students_below_12th_grade
-    sql = "SELECT name FROM students WHERE grade <= 11;"
+    sql = "SELECT students.name FROM students WHERE grade <= 11;"
     DB[:conn].execute(sql)
   end
   
